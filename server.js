263eb5995a2691bcac3ef437b3418a06a77e21bd
Josh Pitman <shidogrey@gmail.com>
2016-02-17 14:30:02 -0500

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    logger  = require('morgan')('dev'),
+    server  = express();
+
+
+server.use(logger);
+server.use(express.static(__dirname+'/public'));
+server.set('port',(process.env.PORT || 8080));
+
+server.get('/', home);
+server.get('/gallery',gallery);
+
+server.listen(server.get('port'), listenCallback);
+
+
+function home(req, res){
+  res.sendFile('public/html/index.html', {root: __dirname});
+}
+
+function gallery(req, res){
+  res.sendFile('public/html/gallery.html', {root: __dirname});
+}
+
+function listenCallback(){
+  console.log('nowlistening on port' + server.get('port'));
+}
